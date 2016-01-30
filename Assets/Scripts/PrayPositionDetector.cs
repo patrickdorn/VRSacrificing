eea8586ffa98642ddba0d4c7af2c39e37c1985df
Patrick Dorn <trash@pamado.de>
2016-01-30 02:40:39 +0100

    added pray prefab

---
+++ Assets/Scripts/PrayPositionDetector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+namespace Jam
+{
+
+    public class PrayPositionDetector : MonoBehaviour
+    {
+
+        public delegate void EnterDelegate(EPrayPosition position);
+        public event EnterDelegate EnterEvent;
+
+        public enum EPrayPosition
+        {
+            Top,
+            Bottom
+        }
+
+        public EPrayPosition position;
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Prayer") && EnterEvent != null)
+            {
+                EnterEvent(position);
+            }
+        }
+    }
+}
