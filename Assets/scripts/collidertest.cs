256f5e4d2b34393f6546c57f313b53a80adb9c24
Dominik Philp <philp.dominik@gmail.com>
2016-01-30 12:58:36 +0100

    basic VR-Interaction
    added standard assets

---
+++ Assets/scripts/collidertest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collidertest : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "controller_LEFT")
+        {
+            Debug.Log("LEFT controller touched the BALL");
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        }
+        else
+        {
+            if (other.tag == "controller_RIGHT")
+            {
+                Debug.Log("RIGHT controller touched the BALL");
+                gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+            }
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "controller_LEFT" || other.tag == "controller_RIGHT")
+        {
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+        }
+    }
+}
---
+++ Assets/scripts/parentingHack.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class parentingHack : MonoBehaviour {
+
+    public Transform parent;
+    public bool ParentPosition;
+    public bool SmoothPosition;
+
+    public bool ParentRotation;
+    public bool SmoothRotation;
+
+    public bool ParentScale;
+    public bool SmoothScale;
+
+    public float SmoothFactor = 0.5f;
+
+    void Update()
+    {
+        if (ParentPosition)
+        {
+            if (SmoothPosition) transform.position = Vector3.Lerp(transform.position, parent.position, SmoothFactor);
+            else transform.position = parent.position;
+        }
+
+        if (ParentRotation)
+        {
+            if (SmoothRotation) transform.rotation = Quaternion.Slerp(transform.rotation, parent.rotation, SmoothFactor);
+            else transform.rotation = parent.rotation;
+        }
+
+        if (ParentScale)
+        {
+            if (SmoothScale) transform.localScale = Vector3.Lerp(transform.localScale, parent.localScale, SmoothFactor);
+            else transform.localScale = parent.localScale;
+
+        }
+    }
+}
