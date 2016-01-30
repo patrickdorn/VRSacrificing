1d2ae26567a79d6f466ce7c3a43817f1cc2ecec2
Dominik Philp <philp.dominik@gmail.com>
2016-01-31 00:03:39 +0100

    implemented hand switching (Prefabs/Hands.prefab)
    implemented terrain blending Scripts/MaterialBlender.cs

---
+++ Assets/Scripts/MaterialBlender.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialBlender : MonoBehaviour {
+
+
+    public bool useBalance = true;
+    [Range(0f, 1f)]
+    public float Blend;
+
+    private Material mat;
+    private Color blendCol = Color.white;
+    private float currentBlend;
+
+    void Awake()
+    {
+        mat = gameObject.GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        if (useBalance) currentBlend = Mathf.Lerp(currentBlend, Mathf.Clamp(Jam.Balance.balance, 0f, 1f), Time.deltaTime);
+        else currentBlend = Mathf.Lerp(currentBlend, Blend, Time.deltaTime);
+
+        blendCol.a = currentBlend;
+        mat.SetColor("_Color", blendCol);
+    }
+}
