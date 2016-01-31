e9ad6810fda2d6fff190f8346e8403dd880140ae
Patrick Dorn <trash@pamado.de>
2016-01-31 13:00:15 +0100

    sound

---
+++ Assets/hackkkkk.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hackkkkk : MonoBehaviour {
+
+    void Awake()
+    {
+
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+        foreach (AudioSource source in GetComponentsInChildren<AudioSource>())
+        {
+            source.Play();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
